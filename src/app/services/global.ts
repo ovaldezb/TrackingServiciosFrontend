0ac0c7e559a8790038d191c0f1ae4972651c029d
ovaldezb <omar.valdez.becerril@gmail.com>
2022-02-03 17:14:28 -0600

    update IP

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,4 @@
 export var Global ={
-    url: 'http://localhost:3899/api/'
-    //url: 'http://192.168.10.10:3899/api/'
+    //url: 'http://localhost:3899/api/'
+    url: 'http://2.1.206.51:3899/api/'
 }
